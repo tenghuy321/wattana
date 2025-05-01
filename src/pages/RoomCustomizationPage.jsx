e9242bf2c13d4cfa52befdb647e96dd56f8720b6
Tenghuy <tenghuyly35@gmailcom>
2025-05-01 15:47:57 +0700

    revised

--- src/pages/RoomCustomizationPage.jsx
+++ src/pages/RoomCustomizationPage.jsx
@@ -16,56 +16,65 @@ import defaultImage from '../assets/images/testing.jpg';
 
 const allImages = {
     'living-room': {
-      livingRoom1: { name: 'Glass Windows', images: [livingRoom1, livingRoom2] },
-      livingRoom2: { name: 'Glass Windows', images: [livingRoom3, livingRoom4] },
-      livingRoom3: { name: 'Glass Windows', images: [livingRoom5, livingRoom6] },
-      livingRoom4: { name: 'Glass Windows', images: [livingRoom1, livingRoom3] },
-      livingRoom5: { name: 'Glass Windows', images: [livingRoom2, livingRoom5] },
-      livingRoom6: { name: 'Glass Windows', images: [livingRoom4, livingRoom6] },
+        livingRoom1: { name: 'Glass Windows', images: [livingRoom1, livingRoom2] },
+        livingRoom2: { name: 'Glass Windows', images: [livingRoom3, livingRoom4] },
+        livingRoom3: { name: 'Glass Windows', images: [livingRoom5, livingRoom6] },
+        livingRoom4: { name: 'Glass Windows', images: [livingRoom1, livingRoom3] },
+        livingRoom5: { name: 'Glass Windows', images: [livingRoom2, livingRoom5] },
+        livingRoom6: { name: 'Glass Windows', images: [livingRoom4, livingRoom6] },
     },
     bedroom: {
-      bedroom1: { name: 'Glass Windows', images: [livingRoom1] },
-      bedroom2: { name: 'Glass Windows', images: [livingRoom2] },
-      bedroom3: { name: 'Glass Windows', images: [livingRoom3] },
-      bedroom4: { name: 'Glass Windows', images: [livingRoom4] },
-      bedroom5: { name: 'Glass Windows', images: [livingRoom5] },
-      bedroom6: { name: 'Glass Windows', images: [livingRoom6] },
+        bedroom1: { name: 'Glass Windows', images: [livingRoom1] },
+        bedroom2: { name: 'Glass Windows', images: [livingRoom2] },
+        bedroom3: { name: 'Glass Windows', images: [livingRoom3] },
+        bedroom4: { name: 'Glass Windows', images: [livingRoom4] },
+        bedroom5: { name: 'Glass Windows', images: [livingRoom5] },
+        bedroom6: { name: 'Glass Windows', images: [livingRoom6] },
     },
     kitchen: {
-      kitchen1: { name: 'Glass Windows', images: [defaultImage] },
-      kitchen2: { name: 'Glass Windows', images: [defaultImage] },
-      kitchen3: { name: 'Glass Windows', images: [defaultImage] },
-      kitchen4: { name: 'Glass Windows', images: [defaultImage] },
-      kitchen5: { name: 'Glass Windows', images: [defaultImage] },
-      kitchen6: { name: 'Glass Windows', images: [defaultImage] },
+        kitchen1: { name: 'Glass Windows', images: [defaultImage] },
+        kitchen2: { name: 'Glass Windows', images: [defaultImage] },
+        kitchen3: { name: 'Glass Windows', images: [defaultImage] },
+        kitchen4: { name: 'Glass Windows', images: [defaultImage] },
+        kitchen5: { name: 'Glass Windows', images: [defaultImage] },
+        kitchen6: { name: 'Glass Windows', images: [defaultImage] },
     },
     'dining-area': {
-      dining1: { name: 'Glass Windows', images: [defaultImage] },
-      dining2: { name: 'Glass Windows', images: [defaultImage] },
-      dining3: { name: 'Glass Windows', images: [defaultImage] },
-      dining4: { name: 'Glass Windows', images: [defaultImage] },
-      dining5: { name: 'Glass Windows', images: [defaultImage] },
-      dining6: { name: 'Glass Windows', images: [defaultImage] },
+        dining1: { name: 'Glass Windows', images: [defaultImage] },
+        dining2: { name: 'Glass Windows', images: [defaultImage] },
+        dining3: { name: 'Glass Windows', images: [defaultImage] },
+        dining4: { name: 'Glass Windows', images: [defaultImage] },
+        dining5: { name: 'Glass Windows', images: [defaultImage] },
+        dining6: { name: 'Glass Windows', images: [defaultImage] },
     },
     balcony: {
-      balcony1: { name: 'Glass Windows', images: [defaultImage] },
-      balcony2: { name: 'Glass Windows', images: [defaultImage] },
-      balcony3: { name: 'Glass Windows', images: [defaultImage] },
-      balcony4: { name: 'Glass Windows', images: [defaultImage] },
-      balcony5: { name: 'Glass Windows', images: [defaultImage] },
-      balcony6: { name: 'Glass Windows', images: [defaultImage] },
+        balcony1: { name: 'Glass Windows', images: [defaultImage] },
+        balcony2: { name: 'Glass Windows', images: [defaultImage] },
+        balcony3: { name: 'Glass Windows', images: [defaultImage] },
+        balcony4: { name: 'Glass Windows', images: [defaultImage] },
+        balcony5: { name: 'Glass Windows', images: [defaultImage] },
+        balcony6: { name: 'Glass Windows', images: [defaultImage] },
     },
-  };
-  
+};
+
 
 const RoomCustomizationPage = () => {
     const { roomType, roomId } = useParams();
     const roomData = allImages[roomType]?.[roomId];
     const images = roomData?.images;
     const roomName = roomData?.name;
-    
+
     return (
         <div className="flex flex-col justify-center items-center w-full h-screen bg-gray-100 pt-20 overflow-hidden">
+            <div className='absolute top-14 md:top-14 lg:top-36 xl:top-40 left-4 md:left-10 xl:left-20 text-black text-[14px] md:text-[16px]'>
+                <a href="/customization" className='border border-[#FF3217] hover:bg-[#FF3217] hover:text-white px-4 py-2 rounded-full flex items-center space-x-2'>
+                    <svg className='w-4 h-4 md:w-6 md:h-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                        <path d="M9 14l-4 -4l4 -4"></path>
+                        <path d="M5 10h11a4 4 0 1 1 0 8h-1"></path>
+                    </svg>
+                    <span>Back</span>
+                </a>
+            </div>
             <h1 data-aos="fade-right" data-aos-duration="1200" className='mb-4 text-xl font-semibold text-gray-700'>{roomName}</h1>
             <div className="w-full max-w-4xl h-1/2 sm:h-2/3 flex justify-center items-center px-4">
                 {images && images.length > 0 ? (
